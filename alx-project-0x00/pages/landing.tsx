17d6ae0ca116b37c6ff983b97aa27e9f930fd945
Bitrus Dauda <bitrusdauda9118@gmail.com>
2025-11-16 22:39:50 +0100

    commit task 5 and 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,19 @@
-const Landing: React.FC = () => {
+import React from "react";
+import Button from "@/components/Button";
+
+const Landing = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Welcome to Landing Page</h1>
+      
+      <div className="flex gap-4 mt-6">
+        <Button title="Small Button" styles="text-sm rounded-sm" />
+        <Button title="Medium Button" styles="text-base rounded-md" />
+        <Button title="Large Button" styles="text-lg rounded-full" />
+      </div>
     </div>
   );
 };
+
 export default Landing;
+
